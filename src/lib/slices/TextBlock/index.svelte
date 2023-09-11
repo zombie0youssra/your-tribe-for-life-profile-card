6b8403a382aab5a774ee15f431791741889b1cd4
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-09-11 13:06:23 +0200

    byeee

--- src/lib/slices/TextBlock/index.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	/** @type {import("@prismicio/client").Content.TextBlockSlice} */
-	import { PrismicRichText } from '@prismicio/svelte';
-	export let slice;
-</script>
-
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	<PrismicRichText field={slice.primary.textblock} />
-</section>
--- src/lib/slices/Title/index.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	/** @type {import("@prismicio/client").Content.TitleSlice} */
-	export let slice;
-</script>
-
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	<h1>{slice.primary.welcome}</h1>
-</section>

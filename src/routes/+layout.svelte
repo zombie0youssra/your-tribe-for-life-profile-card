7cb911dd06bab2b8453215381d42eb736f0fdef8
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-09-15 12:56:08 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,13 @@
-<slot />
+<body>
+	<slot />
+</body>
 
 <style>
+	* {
+		background-color: rgb(26, 30, 48);
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 2rem;
+	}
 </style>

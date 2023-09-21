22d3a5686a723c6a6e3ce4f393d9439d10c09442
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-09-21 21:18:12 +0200

    svg erin

--- src/lib/slices/ListThings/index.svelte
+++ src/lib/slices/ListThings/index.svelte
@@ -6,7 +6,7 @@
 <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
 	{#each slice.items as item (item)}
 		<img src={item.imgproof.url} alt={item.imgproof.url} />
-		<span>{item.info}</span>
+		<!-- <span>{item.info}</span> -->
 		<!-- <a href={item.weblink}>Link</a> -->
 	{/each}
 </section>
--- src/lib/slices/TitleMain/index.svelte
+++ src/lib/slices/TitleMain/index.svelte
@@ -5,7 +5,16 @@
 
 <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
 	<h1>{slice.primary.name}</h1>
+	<svg width="220" height="56" viewBox="5 0 220 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M0 0H55V56H0V0Z" fill="#900E0E" />
+		<path d="M55 0H110V56H55V0Z" fill="#B82808" />
+		<path d="M110 0H165V56H110V0Z" fill="#B85D08" />
+		<path d="M165 0H220V56H165V0Z" fill="#C38C00" />
+	</svg>
 </section>
 
 <style>
+	svg {
+		margin-top: -2rem;
+	}
 </style>
